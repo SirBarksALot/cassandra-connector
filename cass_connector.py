7c599808fc5e0816241decdbb8b6a4e31416ed04
SirBarkALot <filiplez18@interia.pl>
2020-02-08 17:54:24 +0100

    added fct

--- cass_connector.py
+++ cass_connector.py
@@ -1,19 +1,50 @@
 from cassandra.cluster import Cluster
-from cassandra.auth import PlainTextAuthProvider
-import time
+from horology import timed
 
 
-auth_provider = PlainTextAuthProvider(username='cassandra', password='cassandra')
-cluster = Cluster(contact_points=['35.194.33.244'], port=9042, metrics_enabled=True, auth_provider=auth_provider)
-session = cluster.connect()
-session.set_keyspace('graph')
+@timed
+def insert_item(connection_object, var):
+    name = 'wood'
+    unique_name = name + f'_{var}'
+    craftable = True
+    rarity = 'common'
+    requirements = {'tree': 1}
+    connection_object.execute(
+        '''INSERT INTO items (
+    id, name, unique_name, craftable, rarity, requirements, creation_time
+    )
+    VALUES (
+    uuid(), %(name)s, %(unique_name)s, %(craftable)s, %(rarity)s, %(requirements)s, toTimestamp(now())
+    )''',
+        {
+            'name': name,
+            'unique_name': unique_name,
+            'craftable': craftable,
+            'rarity': rarity,
+            'requirements': requirements
+         }
+    )
 
-name = 'wood'
-unique_name = name + '_0'
-craftable = True
-rarity = 'common'
-requirements = {'tree': 1}
-#query = session.execute(f'''CREATE TABLE items(
+
+@timed
+def insert_items(connection_object):
+    amount_to_insert = 15000
+    for i in range(amount_to_insert):
+        insert_item(connection_object, i)
+
+
+@timed
+def connection_handler():
+    cluster = Cluster(contact_points=['35.195.24.202'], port=9042, metrics_enabled=True)
+    session = cluster.connect()
+    session.set_keyspace('logs')
+    insert_items(session)
+    cluster.shutdown()
+
+
+connection_handler()
+
+# query = session.execute(f'''CREATE TABLE items(
 #   id uuid,
 #   name text,
 #   unique_name text,
@@ -22,24 +53,4 @@ requirements = {'tree': 1}
 #   requirements map<text, int>,
 #   creation_time timestamp,
 #   PRIMARY KEY (id, unique_name)
-#)''')
-
-#query = session.execute(
-#    '''INSERT INTO items (
-#    id, name, unique_name, craftable, rarity, requirements, creation_time
-#    )
-#    VALUES (
-#    uuid(), %(name)s, %(unique_name)s, %(craftable)s, %(rarity)s, %(requirements)s, toTimestamp(now())
-#    )''',
-#    {
-#        'name': name,
-#        'unique_name': unique_name,
-#        'craftable': craftable,
-#        'rarity': rarity,
-#        'requirements': requirements
-#     }
-#)
-query = session.execute()
-cluster.shutdown()
-
-print(time.perf_counter())
+# )''')
